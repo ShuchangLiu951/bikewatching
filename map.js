55d95583b4ccedd31011558c9a4cda2306b51104
ShuchangLiu951 <shl153@ucsd.edu>
2025-02-19 16:46:44 -0800

    ...

--- map.js
+++ map.js
@@ -10,3 +10,44 @@ const map = new mapboxgl.Map({
     minZoom: 5, // Minimum allowed zoom
     maxZoom: 18 // Maximum allowed zoom
 });
+
+
+map.on('load', () => {
+    // Add Boston bike lanes as a data source
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+    });
+
+    // Add a layer to visualize bike lanes
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': '#32D400', // Brighter green color
+            'line-width': 5, // Thicker lines
+            'line-opacity': 0.6 // More visible
+        }        
+    });
+
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::RECREATION_BikeFacilities.geojson' // Replace with actual link
+    });
+    
+    map.addLayer({
+        id: 'cambridge-bike-lanes',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+            'line-color': '#FFA500', // Orange for distinction
+            'line-width': 5,
+            'line-opacity': 0.6
+        }
+    });
+    
+
+
+});
+
